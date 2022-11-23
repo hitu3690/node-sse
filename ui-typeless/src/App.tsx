e2fa4b9556af7cc440acd269b541981c2bfd5db9
Yosuke Iida <hitu3690@icloud.com>
2022-11-23 23:49:48 +0900

    index ~ container

--- ui-typeless/src/App.tsx
+++ ui-typeless/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TodoContainer } from "./components/todo";
 import { Route, Routes } from "react-router-dom";
+import { ProjectsIndex } from "./projects/projectsIndex";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/todos" element={<TodoContainer />} />
+        <Route path="/projects" element={<ProjectsIndex />} />
       </Routes>
       <TodoContainer />
     </div>
