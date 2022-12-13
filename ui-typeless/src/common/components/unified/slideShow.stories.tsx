e460951ee23c9471d009fcbf72f7dc95e7ab7157
Yosuke Iida <hitu3690@icloud.com>
2022-12-13 23:11:03 +0900

    slideshow init

---
+++ ui-typeless/src/common/components/unified/slideShow.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { UnifiedSlideShow, UnifiedSlideShowProps } from "./slideShow";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/UnifiedSlideShow",
+  component: UnifiedSlideShow,
+} as ComponentMeta<typeof UnifiedSlideShow>;
+
+export const UnifiedSlideShows = (args: UnifiedSlideShowProps) => {
+  const slideShowItems = [
+    {
+      img: "https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-1/img/img_01.jpg",
+    },
+    {
+      img: "https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-1/img/img_02.jpg",
+    },
+    {
+      img: "https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-1/img/img_03.jpg",
+    },
+  ];
+  return (
+    <>
+      <UnifiedSlideShow
+        title={"SLIDE SHOW"}
+        items={slideShowItems}
+      ></UnifiedSlideShow>
+    </>
+  );
+};
---
+++ ui-typeless/src/common/components/unified/slideShow.tsx
@@ -0,0 +1,64 @@
+import classNames from "classnames";
+import { useState } from "react";
+import styles from "../../styles/unified/slideShow.module.scss";
+
+export interface UnifiedSlideShowProps {
+  className?: string;
+  title?: string;
+  items: {
+    img: string;
+    url?: string;
+  }[];
+  children?: React.ReactNode;
+}
+
+export const UnifiedSlideShow: React.FunctionComponent<
+  UnifiedSlideShowProps
+> = (props) => {
+  const [displayIndex, setDisplayIndex] = useState(0);
+
+  return (
+    <div className={classNames(styles.unifiedSlideShow, props.className)}>
+      <h1>{props.title}</h1>
+      <ul className={styles.slick}>
+        <div
+          className={styles.slickPrev}
+          onClick={() =>
+            setDisplayIndex(
+              displayIndex === 0 ? props.items.length - 1 : displayIndex - 1
+            )
+          }
+        ></div>
+        {props.items.map((item, index) => (
+          <li
+            key={index}
+            style={{
+              background: `url(${item.img})`,
+              display: displayIndex === index ? "block" : "none",
+            }}
+          ></li>
+        ))}
+        <div
+          className={styles.slickNext}
+          onClick={() =>
+            setDisplayIndex(
+              props.items.length - 1 === displayIndex ? 0 : displayIndex + 1
+            )
+          }
+        ></div>
+      </ul>
+      <ul className={styles.slickDots}>
+        {props.items.map((_, index) => (
+          <li key={index}>
+            <button
+              className={
+                displayIndex === index ? styles.currentShow : styles.notShow
+              }
+              onClick={() => setDisplayIndex(index)}
+            ></button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
